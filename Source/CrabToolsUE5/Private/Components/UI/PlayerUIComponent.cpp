67e8000bf7195a0e68239ba4cd20a8da38406b5b
Chris <6255349+DistractionCrab@users.noreply.github.com>
2026-06-07 13:43:15 -0400

    Update PlayerUIComponent.cpp

--- Source/CrabToolsUE5/Private/Components/UI/PlayerUIComponent.cpp
+++ Source/CrabToolsUE5/Private/Components/UI/PlayerUIComponent.cpp
@@ -6,77 +6,94 @@ void UPlayerUIComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	if (IsValid(this->PlayerUIRoot))
-	{
-		this->InitWidget(this->PlayerUIRoot);
-	}
+	#if !UE_SERVER
+		if (IsValid(this->PlayerUIRoot))
+		{
+			this->InitWidget(this->PlayerUIRoot);
+		}
+	#endif
 }
 
 void UPlayerUIComponent::InitWidget(UUserWidget* Widget)
 {
-	if (auto Pawn = Cast<APawn>(this->GetOwner()))
-	{
-		if (auto PC = Pawn->GetLocalViewingPlayerController())
+	#if !UE_SERVER
+		if (auto Pawn = Cast<APawn>(this->GetOwner()))
 		{
-			Widget->SetOwningPlayer(PC);
-			Widget->Initialize();
+			if (auto PC = Pawn->GetLocalViewingPlayerController())
+			{
+				Widget->SetOwningPlayer(PC);
+				Widget->Initialize();
+			}
 		}
-	}
 
-	if (this->bAutomaticallyToViewport)
-	{
-		Widget->AddToPlayerScreen(this->ViewportZOrder);
-	}
+		if (this->bAutomaticallyToViewport)
+		{
+			Widget->AddToPlayerScreen(this->ViewportZOrder);
+		}
 
-	IPlayerUIInterface::Execute_PostInitialize(Widget);
+		IPlayerUIInterface::Execute_PostInitialize(Widget);
+	#endif
 }
 
 void UPlayerUIComponent::SetWidgetClass(TSubclassOf<UUserWidget> WidgetClass)
 {
-	if (this->PlayerUIRoot)
-	{
-		this->PlayerUIRoot->RemoveFromParent();
-		this->PlayerUIRoot = nullptr;
-	}
+	#if !UE_SERVER
+		if (this->PlayerUIRoot)
+		{
+			this->PlayerUIRoot->RemoveFromParent();
+			this->PlayerUIRoot = nullptr;
+		}
 
-	if (auto Pawn = Cast<APawn>(this->GetOwner()))
-	{
-		if (auto PC = Pawn->GetLocalViewingPlayerController())
+
+		if (auto Pawn = Cast<APawn>(this->GetOwner()))
+		{
+			if (auto PC = Pawn->GetLocalViewingPlayerController())
+			{
+				this->PlayerUIRoot = UWidgetBlueprintLibrary::Create(nullptr, WidgetClass, PC);
+			}
+		}
+		else if (auto PC = Cast<APlayerController>(this->GetOwner()))
 		{
 			this->PlayerUIRoot = UWidgetBlueprintLibrary::Create(nullptr, WidgetClass, PC);
 		}
-	}
 
-	if (this->PlayerUIRoot)
-	{
-		this->InitWidget(this->PlayerUIRoot);
-	}
+		if (this->PlayerUIRoot)
+		{
+			this->InitWidget(this->PlayerUIRoot);
+		}
+	#endif
 }
 
 void UPlayerUIComponent::ToggleBindings()
 {
-	if (this->bInputBound)
-	{		
-		UUtilsLibrary::UnbindObjectToEnhancedInput(this->PlayerUIRoot, Cast<APawn>(this->GetOwner()));
-	}
-	else
-	{
-		UUtilsLibrary::BindObjectToEnhancedInput(this->PlayerUIRoot, Cast<APawn>(this->GetOwner()));
-	}
+	#if !UE_SERVER
+		if (this->bInputBound)
+		{		
+			UUtilsLibrary::UnbindObjectToEnhancedInput(this->PlayerUIRoot, Cast<APawn>(this->GetOwner()));
+		}
+		else
+		{
+			UUtilsLibrary::BindObjectToEnhancedInput(this->PlayerUIRoot, Cast<APawn>(this->GetOwner()));
+		}
 		
-	this->bInputBound = !this->bInputBound;
+		this->bInputBound = !this->bInputBound;
+	#endif
 }
 
 UWidget* UPlayerUIComponent::GetUIAs(TSubclassOf<UWidget> WidgetClass, bool& bFound)
 {
-	if (this->PlayerUIRoot && this->PlayerUIRoot->IsA(WidgetClass))
-	{
-		bFound = true;
-		return this->PlayerUIRoot;
-	}
-	else
-	{
-		bFound = false;
+	#if !UE_SERVER
+		if (this->PlayerUIRoot && this->PlayerUIRoot->IsA(WidgetClass))
+		{
+			bFound = true;
+			return this->PlayerUIRoot;
+		}
+		else
+		{
+			bFound = false;
+			return nullptr;
+		}
+	#else
 		return nullptr;
-	}
+	#endif
 }
